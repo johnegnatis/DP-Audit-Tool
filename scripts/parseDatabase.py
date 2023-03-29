de21648ee385a1be82646989ae27e07ed017653d
Haniyyah Hamid <hamidhaniyyahb@gmail.com>
2023-03-29 18:57:26 -0500

    Updated parseDatabase.py

--- scripts/parseDatabase.py
+++ scripts/parseDatabase.py
@@ -5,8 +5,7 @@ import re
 import pandas as pd
 from collections import namedtuple
 
-# TODO: get list of all tracks
-# TODO: separate each cores to all tracks
+
 def parseDatabaseMethod():
     text = ''
     list = namedtuple('data', 'track')
@@ -20,20 +19,16 @@ def parseDatabaseMethod():
     computer_science_tracks = []
     software_engineering_tracks = ['Software Engineering']
     prereqs = []
-    coreCyberSecurity = []
-    coreDataScience = []
-    coreIntelligentSystems = []
-    coreInteractiveComputing = []
-    coreNetworksandTelecommunications = []
-    coreSystems = []
-    coreTraditionalComputerScience = []
-    coreSoftwareEngineering = []
+
     typeOptions = "core"
     list = []
     class_list = []
     dataScience = []
 
+    master_list = []
     masterDict = {}
+    track_name = ""
+               
 
     # LIST OF TRACKS
     for i in range(1,8):
@@ -54,7 +49,6 @@ def parseDatabaseMethod():
 
     # CORES FOR CYBERSECURITY
     for i in range(24,32):
-        #if len(dictionary) == 0:
         dictionary = {
             "course_name": data_list[i],
             "typeOptions": typeOptions
@@ -63,7 +57,6 @@ def parseDatabaseMethod():
         if data_list[i] == "Two of the following courses":
             typeOptions = "two_of_the_following"
             continue
-        #d = {data_list[i] : typeOptions}
         class_list.append(dictionary)
 
     masterDict.update({"cybersecurity" : class_list})
@@ -74,75 +67,117 @@ def parseDatabaseMethod():
     # CORES FOR DATASCIENCE
     dictionary = {}
     for i in range(34,44):
-        if len(dictionary) == 0:
-            dictionary = {
-                data_list[i] : typeOptions
-            }
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
         # ONE OF THE FOLLOWING
         if data_list[i] == "One of the following courses":
             typeOptions = "one_of_the_following"
             continue
-        d = {data_list[i] : typeOptions}
-        class_list.append(d)
+        class_list.append(dictionary)
 
     masterDict.update({"data_science" : class_list})
-    #list.append(masterDict)
     typeOptions = "core"
     dictionary = {}
     class_list = []
 
     # CORES FOR INTELLIGENT SYSTEMS
     for i in range(46,53):
-
-        if len(dictionary) == 0:
-            dictionary = {
-                data_list[i] : typeOptions
-            }
-
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
         # ONE OF THE FOLLOWING
         if data_list[i] == "One of the following courses":
             typeOptions = "one_of_the_following"
             continue
+        class_list.append(dictionary)
 
-        d = {data_list[i] : typeOptions}
-
-        #dictionary.update(d)
-        class_list.append(d)
     masterDict.update({"intelligent_systems" : class_list})
-    #list.append(masterDict)
     typeOptions = "core"
     dictionary = {}
+    class_list = []
 
     # CORES FOR INTERACTIVE COMPUTING
     for i in range(55,64):
-        course = data_list[i]
-        coreInteractiveComputing.append(course)
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
+        # ONE OF THE FOLLOWING
+        if data_list[i] == "Three of the following courses":
+            typeOptions = "three_of_the_following"
+            continue
+        class_list.append(dictionary)
+    masterDict.update({"interactive_computing" : class_list})
+    typeOptions = "core"
+    dictionary = {}
+    class_list = []
+
     # CORES FOR NETWORKS
     for i in range(66,71):
-        course = data_list[i]
-        coreNetworksandTelecommunications.append(course)
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
+        class_list.append(dictionary)
+
+    typeOptions = "core"
+    dictionary = {}
+    class_list = []
+
     # CORES FOR SYSTEMS
     for i in range(73,82):
-        course = data_list[i]
-        coreSystems.append(course)
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
+        # ONE OF THE FOLLOWING
+        if data_list[i] == "One of the following courses":
+            typeOptions = "one_of_the_following"
+            continue
+        class_list.append(dictionary)
+
+    masterDict.update({"systems" : class_list})
+    typeOptions = "core"
+    dictionary = {}
+    class_list = []
+
     # CORES FOR CS
     for i in range(84,91):
-        course = data_list[i]
-        coreTraditionalComputerScience.append(course)    
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
+        # ONE OF THE FOLLOWING
+        if data_list[i] == "Two of the following courses":
+            typeOptions = "two_of_the_following"
+            continue
+        class_list.append(dictionary)
+    masterDict.update({"traditional_computer_science" : class_list})
+    typeOptions = "core"
+    dictionary = {}
+    class_list = []    
+
     # CORES FOR SE
     for i in range(93,98):
-        course = data_list[i]
-        coreSoftwareEngineering.append(course)    
+        dictionary = {
+            "course_name": data_list[i],
+            "typeOptions": typeOptions
+        }
 
-    #print(prereqs)
-    #print(software_engineering_tracks)
-    #for i in range(len(dataScience)):
-     #   print("1:   ") 
-      #  print(dataScience[i])
+        class_list.append(dictionary)
+    masterDict.update({"software_engineering" : class_list})
+    typeOptions = "core"
+    dictionary = {}
+    class_list = []  
+
     jsonStr = json.dumps(masterDict, indent = 4)
     print(jsonStr)
     with open("database.json", "w") as outfile:
         outfile.write(jsonStr)
 
+        
 if __name__ == '__main__':
     parseDatabaseMethod()
