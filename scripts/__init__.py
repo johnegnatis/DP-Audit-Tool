ef758a3d38b6592034a83b36807bc9c777749e7d
johnegnatis <john@wavexchange.team>
2023-04-09 02:47:56 -0500

    integration fix

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,4 +4,4 @@ from .fileSystemInteraction import getFilePathsMethod
 from .fileSystemInteraction import savePDFMethod
 from .doAudit import doAuditMethod
 from .helpers import get_server_path, get_server_name, tree_printer
-from .designateClassesMethod import designateClassesMethod
+from .designateClasses import designateClassesMethod
