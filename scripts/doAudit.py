15519f1511840f26c20638ed34df8b1d91ff1ef0
Jered H <95993968+TheOmega9000@users.noreply.github.com>
2023-04-09 16:32:58 -0500

    Update doAudit.py

--- scripts/doAudit.py
+++ scripts/doAudit.py
@@ -2,6 +2,7 @@ try:
     from scripts.objects import Class, Student, StudentEncoder
 except:
     from objects import Class, Student, StudentEncoder
+import re
 from docx import Document
 from docx.shared import Pt
 from docx.enum.section import WD_SECTION
@@ -10,6 +11,17 @@ from docx.enum.section import WD_SECTION
 def doAuditMethod(studentObject):
 
     print('Starting audit generation...')
+
+    # Class tracking for GPA
+    core_complete = [course for course in studentObject.classes if course.grade and (course.type == 'core' or course.type == 'one_of_the_following')]
+    core_incomplete = [course for course in studentObject.classes if not course.grade and course.type == 'core']
+
+    elective_complete = [course for course in studentObject.classes if course.grade and course.type == 'core_electives']
+    elective_incomplete = [course for course in studentObject.classes if not course.grade and course.type == 'core_electives']
+
+    total_complete = [course for course in studentObject.classes if course.grade and (course.type == 'core' or course.type == 'core_electives' or course.type == 'one_of_the_following')]
+    total_incomplete = [course for course in studentObject.classes if not course.grade and (course.type == 'core' or course.type == 'core_electives')]
+
 
     doc = Document()
     style = doc.styles['Normal']
@@ -25,12 +37,12 @@ def doAuditMethod(studentObject):
     # basic information
     para = doc.add_paragraph()
     para.add_run('Name: ').bold = True
-    para.add_run(studentObject.name)
-    para.add_run('\t\t\t\t\t\tID: ').bold = True
+    para.add_run(studentObject.name.ljust(24,' '))
+    para.add_run('ID: ').bold = True
     para.add_run(str(studentObject.studentId))
     para.add_run('\nPlan: ').bold = True
-    para.add_run('Master')
-    para.add_run('\t\t\t\t\t\t\tMajor: ').bold = True
+    para.add_run('Master'.ljust(24,' '))
+    para.add_run('Major: ').bold = True
     if studentObject.track == 'Software Engineering':
         para.add_run('Software Engineering')
     else:
@@ -39,43 +51,32 @@ def doAuditMethod(studentObject):
     para.add_run(studentObject.track)
 
     #GPA
+    core_gpa = getGPA(core_complete)
+    elective_gpa = getGPA(elective_complete)
+    combined_gpa = getGPA(total_complete)
+
     para = doc.add_paragraph()
     para.add_run('\nCore GPA: ').bold = True
-    para.add_run('call GPA here')
+    para.add_run('%.3f' % core_gpa)
     para.add_run('\nElective GPA: ').bold = True
-    para.add_run('call GPA here')
+    para.add_run('%.3f' % elective_gpa)
     para.add_run('\nCombined GPA: ').bold = True
-    para.add_run('call GPA here')
-
-    core_gpa = 3.734
-    elective_gpa = 3.667
-    combined_gpa = 3.709
+    para.add_run('%.3f' % combined_gpa)
 
     # courses
     para = doc.add_paragraph()
     para.add_run('\nCore Courses: ').bold = True
-    para.add_run(", ".join([course.number for course in studentObject.classes if course.type == 'core']))
+    para.add_run(", ".join([course.number for course in (core_complete + core_incomplete)]))
     para.add_run('\nElective Courses: ').bold = True
-    para.add_run(", ".join([course.number for course in studentObject.classes if course.type == 'core_electives']))
+    para.add_run(", ".join([course.number for course in (elective_complete + elective_incomplete)]))
 
-    # leveling courses (incomplete)
+    # leveling courses (incomplete) NEED LIST OF LEVELING COURSES HERE
     para = doc.add_paragraph()
     para.add_run('\nLeveling Courses and Pre-requisites from Admission Letter:').bold = True
     
     for course in studentObject.classes:
         if course.type == 'prerequisites' and course.grade:
              para.add_run("\n" + course.number + ": Completed" + course.semester)
-
-
-    # Class tracking for GPA
-    core_complete = [course for course in studentObject.classes if course.grade and course.type == 'core']
-    core_incomplete = [course for course in studentObject.classes if not course.grade and course.type == 'core']
-
-    elective_complete = [course for course in studentObject.classes if course.grade and course.type == 'core_electives']
-    elective_incomplete = [course for course in studentObject.classes if not course.grade and course.type == 'core_electives']
-
-    total_complete = [course for course in studentObject.classes if course.grade and (course.type == 'core' or course.type == 'core_electives')]
-    total_incomplete = [course for course in studentObject.classes if not course.grade and (course.type == 'core' or course.type == 'core_electives')]
 
     # requirements (incomplete)
     core_status = ""
@@ -85,11 +86,18 @@ def doAuditMethod(studentObject):
     para = doc.add_paragraph()
     para.add_run('\nOutstanding Requirements:\n').bold = True
 
+    
+    # Taking extra course?
+    extra_course = False
+
     if(len(core_incomplete) == 0):
         core_status = "Core complete."
-    else:
-        core_status = calculateRequiredGPA(3.19, core_gpa, core_complete, core_incomplete)
+    elif(extra_course):
+        core_status = calculateRequiredGPA(3.0, core_gpa, core_complete, core_incomplete)
         para.add_run('\nTo maintain a 3.0 core GPA:')
+    else:
+        ore_status = calculateRequiredGPA(3.19, core_gpa, core_complete, core_incomplete)
+        para.add_run('\nTo maintain a 3.19 core GPA:')
         
     para.add_run('\n' + core_status + ", ".join(course.number for course in core_incomplete))
     
@@ -117,8 +125,61 @@ def doAuditMethod(studentObject):
 def calculateRequiredGPA(required_GPA, GPA, completed_courses, remaining_courses):
     target = (required_GPA * (len(completed_courses) + len(remaining_courses)) - (GPA * len(completed_courses))) / len(remaining_courses)
     if target < 2:
-        return "\tThe student must pass "
-    return "\tThe student needs a GPA of at least %.3f in " % target
+        return "\tThe student must pass: "
+    
+    if remaining_courses == 1:
+        grade = 'Error'
+
+        if target > 4.0:
+            grade = 'Impossible'
+        elif target > 3.670:
+            grade = 'A'
+        elif target > 3.330:
+            grade = 'A-'
+        elif target > 3:
+            grade = 'B+'
+        elif target > 2.670:
+            grade = 'B'
+        elif target > 2.330:
+            grade = 'B-'
+        elif target > 2:
+            grade = 'C+'
+            
+        return "\tThe student needs a grade >=", grade, "in: "
+    
+    return "\tThe student needs a GPA of at least %.3f in: " % target
+
+def letterToGPA(letter):
+    letter = " ".join(re.findall("[a-zA-Z][\+\-]?", letter))
+
+    if letter == 'A':
+        return 4.0
+    if letter == 'A-':
+        return 3.670
+    if letter == 'B+':
+        return 3.330
+    if letter == 'B':
+        return 3.000
+    if letter == 'B-':
+        return 2.670
+    if letter == 'C+':
+        return 2.330
+    if letter == 'C':
+        return 'C-'
+    if letter == 'F':
+        return 0.0
+    
+    return 'Ignore'
+
+def getGPA(completed_courses):
+
+    gpa = 0.0
+    for course in completed_courses:
+        gpa += letterToGPA(course.grade)
+    
+    return gpa / len(completed_courses)
+
+
 
 # test case
 classes = [
