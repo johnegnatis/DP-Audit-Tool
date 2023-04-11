6df6f56e013d7e6044df1b24064c6280f547dc41
johnegnatis <john@wavexchange.team>
2023-04-11 15:24:36 -0500

    Designate Classes Merge

--- scripts/designateClasses.py
+++ scripts/designateClasses.py
@@ -10,6 +10,16 @@ import json
 import pandas as pd
 import numpy as np
 
+import json
+import pandas as pd
+import numpy as np
+
+coreKey = 'core'
+followingKey = 'following'
+prerequisiteKey = 'prerequisites'
+electiveKey = 'electives'
+unsureKey = 'unsure'
+
 def test_strings(database_string, transcript_string):
     if (database_string == ''): return False
     if (transcript_string == ''): return False
@@ -23,25 +33,28 @@ def grade_key(obj):
         return grades.index('')
 
 def type_key(obj):
-    types = ['core', 'following', 'electives', 'prerequisites', 'unsure', '']  
+    types = [coreKey, followingKey, electiveKey, prerequisiteKey, unsureKey, '']  
     try:
         return types.index(obj.type)
     except ValueError:
         return types.index('')
 
-# TODO: test on class ID, not on name
+# FINDS CLASS IN ONE OF THE TYPELISTS, CHANGES NAME
+def assign_name(class_name, typeList):
+    for i in range(0, len(typeList)):
+        if (test_strings(class_name.number, typeList[i]['id'])): # exists in typeList, replace studentObj's name w/ typeList[name] and go back
+            class_name.name = typeList[i]['name']
+            break
+
+
 def find_add_classes(newClasses, classList, typeList, typeKey):
-
-    # TODO: NIYA: change the test_strings method to check classList[i].number, typeList[j]['id']
-    #              - Make sure to format the string so the test does not fail i.e. remove spaces,
-    #               anything that is not number or letter. make all letters uppercase, ext..
-
     found = False
     for j in range(0, len(typeList)):
         for i in range(0, len(classList)):
             found = False
             if (test_strings(classList[i].number, typeList[j]['id'])): # finds class w/i database, assigns type
                 classList[i].type = typeKey
+                classList[i].name = typeList[j]['name'] # assign name from DB here
                 found = True
                 break
         if not found: # student hasn't take the class yet, store as a new class to add to classes[] list later
@@ -58,12 +71,8 @@ def designateClassesMethod(studentObject):
     classes = []
     track_name = ''
     cores = []
-    coreKey = 'core'
     following = []
-    followingKey = 'following'
     prerequisites = []
-    prerequisiteKey = 'prerequisites'
-    electiveKey = 'electives'
     trackList = data['tracks']
     num_of_following = 0 
 
@@ -82,43 +91,37 @@ def designateClassesMethod(studentObject):
     find_add_classes(newClasses, studentObject.classes, following, followingKey)
     find_add_classes(newClasses, studentObject.classes, prerequisites, prerequisiteKey)
 
-    for i in range(0, len(studentObject.classes)): # update all untyped to 'elective'
-        if (test_strings(studentObject.classes[i].type, 'unsure')):
-            studentObject.classes[i].type = 'electives'    
+    for i in range(0, len(studentObject.classes)): # update all untyped to 'electives'
+        if (test_strings(studentObject.classes[i].type, unsureKey)):
+            studentObject.classes[i].type = electiveKey    
 
     # Combine the classes
     studentObject.classes = studentObject.classes + newClasses
 
     # CHANGING THE CAPITALS - electives won't be in DB
     for i in range(0, len(studentObject.classes)):
-        if studentObject.classes[i].type != 'elective': # current class type != elective, find in typeList and replace
-            if studentObject.classes[i].type == coreKey:
-                assign_name(studentObject.classes[i], cores)
-            if studentObject.classes[i].type == followingKey:
-                assign_name(studentObject.classes[i], following)
-            if studentObject.classes[i].type == prerequisiteKey:
-                assign_name(studentObject.classes[i], prerequisites)
-        else: # current class type IS an elective -> manually change
+        if studentObject.classes[i].type == electiveKey: # current class type == elective, make it title case; non-electives are handled above
             studentObject.classes[i].name = (studentObject.classes[i].name).title()
-    
-    # N-OF-FOLLOWING TYPE ASSIGNMENT
-    following_list = []
-    for i in range(0, len(studentObject.classes)): # add all 'following' types to a new list
-        if studentObject.classes[i].type == 'following':
-            following_list.append(Class(studentObject.classes[i].name, studentObject.classes[i].number, studentObject.classes[i].semester, studentObject.classes[i].transfer, studentObject.classes[i].grade, studentObject.classes[i].attempted_credits, followingKey))
 
-    following_list = sorted(following_list, key=grade_key) # sort new list
-        
-    following_assign(following_list, num_of_following, followingKey) # assign 'following's and 'electives
-
-    for i in range(0, len(following_list)): # add back to studentObject.classes[]
-        for j in range(0, len(studentObject.classes)):
-            if studentObject.classes[j].type == followingKey and studentObject.classes[j].number == following_list[i].number:
-                studentObject.classes[j] = following_list[i]
-                break
-
-    # Sorting classes
+    # Sorting by grade
     studentObject.classes = sorted(studentObject.classes, key=grade_key)
+    
+    # IF the student has completed 2 of the following courses but the track only calls for 1,  
+    # then the higher grade one should stay and the lower grade one goes into electives.
+    count = 0
+    for classObj in studentObject.classes:
+        # only for following classes that have grades
+        if classObj.type != followingKey or not classObj.grade:
+            continue
+        
+        if count >= num_of_following: # if we have surpassed the limit of following classes, this following class becomes an elective
+            classObj.type = electiveKey
+        elif classObj.grade:
+            count = count + 1
+
+    # Here is where to divide by additional and electives
+    
+    # Sorting by type
     studentObject.classes = sorted(studentObject.classes, key=type_key)
 
     for i in range(0, len(studentObject.classes)):
